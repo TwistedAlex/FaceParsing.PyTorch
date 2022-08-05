26788cf14d407b1d1c3f21e6c438e348efdbf748
TwistedAlex <muzihuole@gmail.com>
2022-08-05 11:55:16 -0500

    remove inplace_abn

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1 python -u main.py --arch FaceParseNet50 --train False --pretrained_model 38 --test_image_path test_images/
+CUDA_VISIBLE_DEVICES=$0 python -u main.py --arch FaceParseNet50 --train False --pretrained_model 38 --test_image_path test_images/
